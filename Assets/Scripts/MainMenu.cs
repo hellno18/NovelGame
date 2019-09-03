0628ea1bcb7608399e7865a2723d45ec6c2e4d42
Michael Jack <40555371+hellno18@users.noreply.github.com>
2019-09-03 12:31:43 +0900

    Added option menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -9,27 +9,46 @@ public class MainMenu : MonoBehaviour
 {
     private int _indexBGM;
     private int _indexBG;
+    private float bgm=0.5f;
+    private float sfx=0.5f;
     Image background;
     Button start;
     Button credit;
     ScenarioResource resource;
     Image panel;
+    Image panelOption;
     AudioSource audioSource;
     AudioClip audioClip;
+    Slider sliderBGM;
+    Slider sliderSFX;
     private void Awake()
     {
+        bgm = PlayerPrefs.GetFloat("bgm");
+        sfx = PlayerPrefs.GetFloat("sfx");
+
         _indexBGM = 12;
         PlayerPrefs.SetInt("MiniGame", 0);
         start = this.transform.Find("StartButton").GetComponent<Button>();
         credit = this.transform.Find("CreditButton").GetComponent<Button>();
         background = this.transform.Find("BG").GetComponent<Image>();
         panel = this.transform.Find("Panel").GetComponent<Image>();
+        panelOption= this.transform.Find("PanelOption").GetComponent<Image>();
+        sliderBGM = GameObject.Find("SliderBGM").GetComponent<Slider>();
+        sliderSFX = GameObject.Find("SliderSFX").GetComponent<Slider>();
+
+        sliderBGM.value = bgm;
+        sliderSFX.value = sfx;
+
         audioSource = this.GetComponent<AudioSource>();
         resource = ScenarioResource.GetInstace();
         resource.Load();
         PlayBGM();
     }
-    
+    public void VolumeBGMController()
+    {
+        //sliderBGM.value = ;
+    }
+
     public void StartMenu()
     {
         SceneManager.LoadScene("LoadingScreen");
@@ -43,10 +62,9 @@ public class MainMenu : MonoBehaviour
         start.gameObject.SetActive(false);
         credit.gameObject.SetActive(false);
         panel.gameObject.SetActive(true);
-
     }
 
-    public void BackButton()
+    public void BackButtonCredit()
     {
         _indexBGM = 12;
         _indexBG = 18;
@@ -55,6 +73,23 @@ public class MainMenu : MonoBehaviour
         start.gameObject.SetActive(true);
         credit.gameObject.SetActive(true);
         panel.gameObject.SetActive(false);
+    }
+    public void OptionButton()
+    {
+        _indexBGM = 9;
+        _indexBG = 20;
+        ChangeBackground();
+        PlayBGM();
+        start.gameObject.SetActive(false);
+        credit.gameObject.SetActive(false);
+        panelOption.gameObject.SetActive(true);
+    }
+
+    public void BackButtonOption()
+    {
+        start.gameObject.SetActive(true);
+        credit.gameObject.SetActive(true);
+        panelOption.gameObject.SetActive(false);
     }
     private void PlayBGM()
     {
